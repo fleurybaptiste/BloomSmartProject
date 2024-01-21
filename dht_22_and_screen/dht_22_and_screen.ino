3f2f9034df2b5e7e5ea40ba7b96640942a206f70
Baptiste Fleury <fleury.baptiste.oclock@gmail.com>
2024-01-21 12:15:57 +0100

    All folders

---
+++ dht_22_and_screen/dht_22_and_screen.ino
@@ -0,0 +1,119 @@
+#include <dummy.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+#define SCREEN_WIDTH 128 // Largeur de l'écran OLED en pixels
+#define SCREEN_HEIGHT 32 // Hauteur de l'écran OLED en pixels
+#define OLED_RESET     -1 // Pas utilisé
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define SDA_PIN 19
+#define SCL_PIN 22
+
+#define DHTPIN D5     // Broche où est connecté le DHT22
+#define DHTTYPE DHT22   // DHT 22 (AM2302)
+
+const char* ssid = "Livebox-E190";
+const char* password = "AE64iEHxDuw5NpCFfE";
+const char* serverUrl = "http://192.168.1.20:3000/sensor-data";
+
+
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+
+
+void setup() {
+    Serial.begin(115200);
+
+WiFi.begin(ssid, password);
+Serial.println("Tentative de connexion au Wi-Fi...");
+
+while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+}
+
+Serial.println("");
+Serial.println("Connecté au Wi-Fi.");
+Serial.print("Adresse IP: ");
+Serial.println(WiFi.localIP());
+
+    // Initialisation de l'écran OLED
+    Wire.begin(SDA_PIN, SCL_PIN); // Initialise la communication I2C
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Boucle infinie en cas d'échec
+    }
+    display.clearDisplay();
+
+    // Initialisation du capteur DHT
+    dht.begin();
+}
+
+void loop() {
+// Lire l'humidité et la température
+float humidity = dht.readHumidity();
+float temperature = dht.readTemperature();
+
+// Utiliser Serial.print pour afficher les lectures sur le moniteur série
+if (isnan(humidity) || isnan(temperature)) {
+Serial.println(F("Failed to read from DHT sensor!"));
+} else {
+// Afficher les lectures sur le moniteur série
+Serial.print(F("Humidity: "));
+Serial.print(humidity);
+Serial.print(F("%, Temperature: "));
+Serial.print(temperature);
+Serial.println(F("C"));
+}
+
+// Afficher les lectures sur l'écran OLED
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(SSD1306_WHITE);
+display.setCursor(0,0);
+display.print(F("Temp: "));
+display.print(temperature);
+display.println(F("C"));
+display.setCursor(0, 10);
+display.print(F("Humid: "));
+display.print(humidity);
+display.println(F("%"));
+display.display();
+
+delay(2000);
+
+if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(client, serverUrl); // Commencez la connexion avec le client WiFi
+
+    http.addHeader("Content-Type", "application/json");
+
+    // Créez l'objet JSON pour l'envoi
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["temperature"] = temperature;
+    jsonDoc["humidity"] = humidity;
+    String requestBody;
+    serializeJson(jsonDoc, requestBody);
+
+    // Envoyez la requête POST
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println(response);
+    } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end(); // Terminez la connexion
+}
+}
